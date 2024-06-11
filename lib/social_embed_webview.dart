3d08de802134471c1c6e9cee5fc7202a96b8fc3f
jigaradesarasa <121226160+jigaradesarasa@users.noreply.github.com>
2024-06-11 12:59:42 +0530

    User agent web set social_embed_webview.dart

--- lib/social_embed_webview.dart
+++ lib/social_embed_webview.dart
@@ -5,6 +5,7 @@ import 'package:social_embed_webview/platforms/social-media-generic.dart';
 import 'package:social_embed_webview/utils/common-utils.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
 
 class SocialEmbed extends StatefulWidget {
   final SocialMediaGenericEmbedData socialMediaObj;
@@ -56,6 +57,7 @@ class _SocialEmbedState extends State<SocialEmbed> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     final wv = WebView(
         initialUrl: htmlToURI(getHtmlBody()),
+        userAgent: Platform.isIOS ? "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217":"Mozilla/5.0 (Linux; Android 5.1.1; Android SDK built for x86 Build/LMY48X) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36"
         javascriptChannels:
             <JavascriptChannel>[_getHeightJavascriptChannel()].toSet(),
         javascriptMode: JavascriptMode.unrestricted,
